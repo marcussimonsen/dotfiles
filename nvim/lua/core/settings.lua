e71a082d3949ce44ce227d4824fa29e12fd05f03
Marcus Henrik Simonsen <marcus@itis.dk>
2024-03-05 15:32:42 +0100

    Expand tab

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -5,7 +5,7 @@ vim.o.relativenumber = true
 -- Indentation settings
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.expandtab = false
+vim.o.expandtab = true
 
 -- Scroll before cursor reaches screen bounds
 vim.o.scrolloff = 7
