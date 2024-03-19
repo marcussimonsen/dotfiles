9055de32e63fc69a97631b115ba957b3aa54f503
Marcus Henrik Simonsen <marcus@itis.dk>
2024-03-19 09:15:39 +0100

    Better keymaps and Which-key plugin

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,12 +1,29 @@
+local wk = require('which-key')
+
+local telescope = require('telescope.builtin')
+
+wk.register({
+    f = { -- Telescope
+        name = 'Telescope',
+        f = { telescope.find_files, "Find File" },
+        r = { telescope.oldfiles, "Recent Files" },
+        g = { telescope.live_grep, "Live Grep" },
+        b = { telescope.buffers, "Buffers" },
+        h = { telescope.help_tags, "Help Tags" },
+        c = { telescope.commands, "Commands" },
+        q = { telescope.quickfix, "Quickfix" },
+        t = { telescope.treesitter, "Treesitter" },
+    },
+    g = { -- Fugitive
+        name = "Git (Fugitive)",
+        d = { "<cmd>Gdiffsplit<CR>", "Git Diff Split" },
+        g = { "<cmd>Git<CR>", "Git Summary" },
+    }
+}, { prefix = "<Leader>" })
+
 -- Generic
 vim.keymap.set('n', '<Leader>n', ':noh<CR>')
 
 -- Vimwiki
 vim.keymap.set('n', '<Leader>vtt', ':VimwikiTable<CR>')
 vim.keymap.set('n', '<Leader>vt ', ':VimwikiTable ')
-
--- Telescope
-vim.keymap.set('n', '<Leader>f', ':Telescope find_files<CR>')
-
--- Fugitive
-vim.keymap.set('n', '<Leader>gd', ':Gdiffsplit<CR>')
