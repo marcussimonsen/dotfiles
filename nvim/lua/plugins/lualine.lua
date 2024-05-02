b205f65cf7ab44c20e6f1d68a04bb0561a9d8084
Marcus Henrik Simonsen <marcus@itis.dk>
2024-05-02 17:14:32 +0200

    Tabline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,11 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require('lualine').setup({})
+        require('lualine').setup({
+            tabline = {
+                lualine_a = { 'buffers' },
+                lualine_z = { 'tabs' },
+            }
+        })
     end
 }
