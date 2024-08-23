dd60ef363538ee8e92d85e52522dc9b42ff7168e
Marcus Henrik Simonsen <marcus@itis.dk>
2024-08-23 10:08:28 +0200

    Add auto creation of symbolic links

---
+++ create_symbolic_links.sh
@@ -0,0 +1,7 @@
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/waybar ~/.config/waybar
+ln -s ~/dotfiles/wezterm ~/.config/wezterm
+ln -s ~/dotfiles/kitty ~/.config/kitty
+ln -s ~/dotfiles/hyprland ~/.config/hyprland
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
