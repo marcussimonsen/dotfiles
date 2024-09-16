ad93da59190f73a7a90f0963e0c45a6035a123a9
Marcus Henrik Simonsen <mhsi@itu.dk>
2024-09-16 21:52:56 +0200

    NVim Colorschemes in 1 file

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,6 @@
+return {
+    "Mofiqul/dracula.nvim",
+    "rebelot/kanagawa.nvim",
+    { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}
