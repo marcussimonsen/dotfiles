361d5da6b675964408f363346ddb772d1c4f9754
Marcus Henrik Simonsen <mhsi@itu.dk>
2024-10-04 13:19:32 +0200

    Formatter fixes

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,17 +1,20 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
+	event = { "BufWritePre" },
 	config = function()
 		require("conform").setup({
-			lsp_format = "first",
+			default_format_opts = {
+				lsp_format = "first",
+			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "autoflake", "black" },
 				c = { "clang-format" },
 				cpp = { "clang-format" },
 				haskell = { "stylish-haskell" },
-				markdown = { "markdownfmt", "cbfmt", "mdslw" },
-				scala3 = { "scalafmt" },
+				markdown = { --[["cbfmt",]]
+					"mdslw",
+				},
 				rust = { "rustfmt" },
 			},
 		})
