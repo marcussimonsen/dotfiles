9581f905a23f3336f42132297fd1a2e14ee0ca08
Marcus Henrik Simonsen <mhsi@itu.dk>
2024-10-04 13:20:03 +0200

    Update lazylock
    Set colorscheme in settings
    
    Set colorscheme not here

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,6 @@ require("core.lazy")
 require("core.settings")
 require("core.keymaps")
 
-vim.cmd.colorscheme 'catppuccin-mocha'
 -- Use the following to change to darker theme at night
 -- local time = os.date("*t")
 -- if time.hour >= 17 or time.hour < 7 then
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,6 +3,9 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('lualine').setup({
+            options = {
+                theme = "catppuccin",
+            },
             tabline = {
                 lualine_a = { 'buffers' },
                 lualine_y = { 'tabs' },
