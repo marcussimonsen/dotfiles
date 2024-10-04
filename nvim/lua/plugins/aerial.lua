46ecf80a4be8d941af243d8bb1bea0fa85ddc5d2
Marcus Henrik Simonsen <mhsi@itu.dk>
2024-10-04 13:23:05 +0200

    NeoVim: Some fixes to Aerial plugin

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -5,10 +5,13 @@ return {
         'nvim-tree/nvim-web-devicons',
         'nvim-telescope/telescope.nvim',
     },
+    lazy_load = true,
     config = function()
         require('aerial').setup({
             close_automatic_events = { 'unsupported' },
             open_automatic = false,
+            autojump = true,
+            filter_kind = false,
         })
         require('telescope').load_extension('aerial')
     end
