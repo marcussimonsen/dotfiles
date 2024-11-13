330ec0384daf91b4a4206cf53556d24c5bc10ecd
Marcus Henrik Simonsen <mhsi@itu.dk>
2024-11-13 18:43:52 +0100

    NVim: Fix trouble keymaps

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,25 +1,25 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "BufRead",
+	event = "BufRead",
 	config = function()
 		local util = require("core.utilities")
 		local trouble = require("trouble")
 
-		local function lazy_toggle(x)
-			return util.lazy_eval(trouble.toggle, x)
+		local function open(x)
+			return util.lazy_eval(trouble.open, x)
 		end
 
 		trouble.setup({ focus = true })
 
-        vim.keymap.set("n", "<Leader>xt", vim.cmd.TodoTrouble, { desc = "ToDo Trouble" })
-		vim.keymap.set("n", "ga", lazy_toggle("diagnostics"), { desc = "Diagnostics" })
-		vim.keymap.set("n", "<Leader>xl", lazy_toggle("loclist"), { desc = "Loclist" })
-		vim.keymap.set("n", "<Leader>xq", lazy_toggle("quickfix"), { desc = "Quickfix" })
-		vim.keymap.set("n", "gr", lazy_toggle("lsp_references"), { desc = "LSP References" })
-		vim.keymap.set("n", "gi", lazy_toggle("lsp_implementations"), { desc = "LSP Implementations" })
-		vim.keymap.set("n", "gd", lazy_toggle("lsp_definitions"), { desc = "LSP Definitions" })
-		vim.keymap.set("n", "gD", lazy_toggle("lsp_declarations"), { desc = "LSP Definitions" })
-		vim.keymap.set("n", "<Leader>xs", lazy_toggle("symbols"), { desc = "Symbols" })
+		vim.keymap.set("n", "<Leader>xt", vim.cmd.TodoTrouble, { desc = "ToDo" })
+		vim.keymap.set("n", "<Leader>xd", open("diagnostics"), { desc = "Diagnostics" })
+		vim.keymap.set("n", "<Leader>xl", open("loclist"), { desc = "Loclist" })
+		vim.keymap.set("n", "<Leader>xq", open("quickfix"), { desc = "Quickfix" })
+		vim.keymap.set("n", "gr", open("lsp_references"), { desc = "LSP References" })
+		vim.keymap.set("n", "gi", open("lsp_implementations"), { desc = "LSP Implementations" })
+		vim.keymap.set("n", "gd", open("lsp_definitions"), { desc = "LSP Definitions" })
+		vim.keymap.set("n", "gD", open("lsp_declarations"), { desc = "LSP Definitions" })
+		vim.keymap.set("n", "<Leader>xs", open("symbols"), { desc = "Symbols" })
 	end,
 }
