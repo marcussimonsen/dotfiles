3659f4631516a3238aa73f0915fd1e52daa99997
Marcus Henrik Simonsen <mhsi@itu.dk>
2025-09-02 16:40:29 +0200

    NeoVim: Add Fzf plugin

---
+++ config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,155 @@
+return {
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- config = function()
+	-- 	require("fzf-lua").setup({
+	-- 		keymap = {
+	-- 			fzf = {
+	-- 				true,
+	-- 				["alt-t"] = function()
+	-- 					require("trouble.sources.fzf").open()
+	-- 				end,
+	-- 			},
+	-- 		},
+	-- 	})
+	-- end,
+	cmd = "FzfLua",
+	keys = {
+		{
+			"<Leader>s<Leader>",
+			mode = "n",
+			function()
+				require("fzf-lua").builtin()
+			end,
+			desc = "Fzf Commands",
+		},
+		{
+			"<Leader>sb",
+			mode = "n",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<Leader>sc",
+			mode = "n",
+			function()
+				require("fzf-lua").changes()
+			end,
+			desc = "Changes",
+		},
+		{
+			"<Leader>sC",
+			mode = "n",
+			function()
+				require("fzf-lua").commands()
+			end,
+			desc = "Commands",
+		},
+		{
+			"<Leader>sf",
+			mode = "n",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "Find File",
+		},
+		{
+			"<Leader>sg",
+			mode = "n",
+			function()
+				require("fzf-lua").live_grep()
+			end,
+			desc = "Live Grep",
+		},
+		{
+			"<Leader>sG",
+			mode = "n",
+			function()
+				require("fzf-lua").grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<Leader>sk",
+			mode = "n",
+			function()
+				require("fzf-lua").keymaps()
+			end,
+			desc = "Keymaps",
+		},
+		{
+			"<Leader>sh",
+			mode = "n",
+			function()
+				require("fzf-lua").helptags()
+			end,
+			desc = "Help Tags",
+		},
+		{
+			"<Leader>sm",
+			mode = "n",
+			function()
+				require("fzf-lua").marks()
+			end,
+			desc = "Marks",
+		},
+		{
+			"<Leader>sp",
+			mode = "n",
+			function()
+				require("fzf-lua").colorschemes()
+			end,
+			desc = "Colorscheme",
+		},
+		{
+			"<Leader>sq",
+			mode = "n",
+			function()
+				require("fzf-lua").quickfix()
+			end,
+			desc = "Quickfix",
+		},
+		{
+			"<Leader>st",
+			mode = "n",
+			function()
+				require("fzf-lua").treesitter()
+			end,
+			desc = "Treesitter",
+		},
+		{
+			"<Leader>sd",
+			mode = "n",
+			function()
+				require("fzf-lua").lsp_document_symbols()
+			end,
+			desc = "LSP Document Symbols",
+		},
+		{
+			"<Leader>sw",
+			mode = "n",
+			function()
+				require("fzf-lua").lsp_workspace_symbols()
+			end,
+			desc = "LSP Document Symbols",
+		},
+		{
+			"<Leader>sl",
+			mode = "n",
+			function()
+				require("fzf-lua").loclist()
+			end,
+			desc = "Loclist",
+		},
+		{
+			"<Leader>sw",
+			mode = "n",
+			function()
+				require("fzf-lua").grep_cword()
+			end,
+			desc = "Under Cursor",
+		},
+	},
+}
